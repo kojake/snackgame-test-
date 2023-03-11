a5d9800ead1b5ccce9d65688a1d936dd9bac943c
kaito <takahashikaito@kaitonoMacBook-Air.local>
2023-03-11 10:00:46 +0900

    update

--- game_test/homeview.swift
+++ game_test/homeview.swift
@@ -13,11 +13,24 @@ class homeview: UIViewController {
         
         level_label.text = String(level)
         diamond_quantity_label.text = String(diamond)
+        shop_gift_label.text = "SHOP"
     }
 
     //item
     @IBOutlet weak var level_label: UILabel!
     @IBOutlet weak var level_progress: UIProgressView!
     @IBOutlet weak var diamond_quantity_label: UILabel!
+    
+    //shop_gift_view
+    @IBOutlet weak var shop_gift_view: UIView!
+    @IBOutlet weak var shop_gift_label: UILabel!
+    @IBAction func GIFT_button(_ sender: UIButton) {
+        shop_gift_label.text = "GIFT"
+        sender.tintColor = UIColor.systemRed
+    }
+    @IBAction func SHOP_button(_ sender: UIButton) {
+        shop_gift_label.text = "SHOP"
+        sender.tintColor = UIColor.systemRed
+    }
     
 }
